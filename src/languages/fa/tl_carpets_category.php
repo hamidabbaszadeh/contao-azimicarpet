5d5e002dba8cbf47b9d8b21c5ef9ac0c95b86890
Hamid Abbaszadeh <abbaszadeh.h@gmail.com>
2018-08-25 16:34:32 +0430

    add some new legends

--- src/languages/fa/tl_carpets_category.php
+++ src/languages/fa/tl_carpets_category.php
@@ -5,6 +5,10 @@
  */
 $GLOBALS['TL_LANG']['tl_carpets_category']['title']       = array('عنوان', 'لطفا عنوان دسته‌بندی را وارد نمایید .');
 $GLOBALS['TL_LANG']['tl_carpets_category']['jumpTo']      = array('صفحه بازگردانی','لطفا صفحه‌ای را که می‌خواهید کاربر بعد از کلیک بر روی کالا به آنجا منتقل شود را انتخاب نمایید.');
+$GLOBALS['TL_LANG']['tl_carpets_category']['price_1_inc'] = array('درصد افزایش قیمت نخ و نقشه', '');
+$GLOBALS['TL_LANG']['tl_carpets_category']['price_2_inc'] = array('درصد افزایش قیمت نخ و نقشه + دار', '');
+$GLOBALS['TL_LANG']['tl_carpets_category']['price_3_inc'] = array('درصد افزایش قیمت نخ و نقشه با چله ابریشم', '');
+$GLOBALS['TL_LANG']['tl_carpets_category']['price_4_inc'] = array('درصد افزایش قیمت نخ و نقشه با چله ابریشم + دار', '');
 $GLOBALS['TL_LANG']['tl_carpets_category']['protected']   = array('حفاظت از دسته‌بندی','با فعال کردن این گزینه می‌توانید به گروه خاصی اجازه مشاهده کالاها را بدهید.');
 $GLOBALS['TL_LANG']['tl_carpets_category']['groups']      = array('گروه‌های دارای دسترسی','These groups will be able to see the carpet category.');
 
@@ -15,6 +19,7 @@ $GLOBALS['TL_LANG']['tl_carpets_category']['groups']      = array('گروه‌ه
 $GLOBALS['TL_LANG']['tl_carpets_category']['title_legend']     = 'نام و اسم مستعار';
 $GLOBALS['TL_LANG']['tl_carpets_category']['redirect_legend']  = 'تنظیمات بازگردانی';
 $GLOBALS['TL_LANG']['tl_carpets_category']['protected_legend'] = 'حفاظت دسترسی';
+$GLOBALS['TL_LANG']['tl_carpets_category']['price_inc_legend'] = 'مقدار افزایش قیمت این دسته';
 
 
 /**
